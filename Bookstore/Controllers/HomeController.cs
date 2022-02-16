d705ed2c0e2fc23694cd052824878b11bf80d94e
mperry5th <90285075+mperry5th@users.noreply.github.com>
2022-02-16 11:11:26 -0700

    Add project files.

---
+++ Bookstore/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Bookstore.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Bookstore.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IBookstoreRepository repo;
+        public HomeController(IBookstoreRepository temp) => repo = temp;
+        public IActionResult Index()
+        {
+            var book_data = repo.Books
+                .OrderBy(b => b.Title);
+            return View(book_data);
+        }
+
+    }
+}
